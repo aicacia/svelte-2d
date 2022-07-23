0b56e82daa5bcb5de20ddff4881a571bffe9f05b
Nathan Faucett <nathanfaucett@gmail.com>
2022-07-23 08:49:49 -0400

    fix warnings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
-import fs from 'fs';
-
-const pkg = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf8'));
 
 /** @type {import('vite').UserConfig} */
 const config = {
